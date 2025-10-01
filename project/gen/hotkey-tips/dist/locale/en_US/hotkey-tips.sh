ed7a3c9a320f481d102d63b56daf1fbee4dbaf4a
samwhelp <samwhelp@users.noreply.github.com>
2025-10-01 21:30:50 +0800

    fix

---
+++ project/gen/hotkey-tips/dist/locale/en_US/hotkey-tips.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+yad --width=1000 --height=600 \
+--center \
+--title="Popos Cosmic Keybindings" \
+--no-buttons \
+--list \
+--column=Keybind: \
+--column=Action: \
+--column=@fore@ \
+--column=@font@ \
+--timeout=15 \
+--timeout-indicator=right \
+"" "" "#6500c1" "Monospace 1" \
+"" "Cheatsheet will be closed in 15 seconds!" "#ff0000" "Monospace 16" \
+""  "ESC to close this cheatsheet" "#00ffff" "Monospace 16" \
+"Alt + Shift + x" "Logout" "#ffffff" "Monospace 16" \
+"Alt + Shift + z" "Leave" "#cccccc" "Monospace 16" \
+"Alt + Shift + l" "Lock Screen" "#ffffff" "Monospace 16" \
+"Alt + F1" "Main Menu" "#cccccc" "Monospace 16" \
+"Alt + F2" "Runner" "#ffffff" "Monospace 16" \
+"Alt + Shift + f" "Launch File Manager (cosmic-files)" "#cccccc" "Monospace 16" \
+"Alt + Shift + g" "Launch File Manager (thunar)" "#ffffff" "Monospace 16" \
+"Alt + Shift + e" "Launch Text Editor (cosmic-edit)" "#cccccc" "Monospace 16" \
+"Alt + Shift + b" "Launch Web Browser (firefox)" "#ffffff" "Monospace 16" \
+"Alt + Shift + s" "Launch System Setting (cosmic-settings)" "#cccccc" "Monospace 16" \
+"Alt + Enter" "Launch Terminal (cosmic-term)" "#ffffff" "Monospace 16" \
+"Alt + Shift + a" "Launch Terminal (cosmic-term)" "#cccccc" "Monospace 16" \
+"Alt + Ctrl + a" "Launch Terminal (xfce4-terminal)" "#ffffff" "Monospace 16" \
+"Alt + Shift + t" "Launch Terminal (qterminal)" "#cccccc" "Monospace 16" \
+"Alt + Ctrl + t" "Launch Terminal (lxterminal)" "#ffffff" "Monospace 16" \
+"Win + q" "Window Close" "#cccccc" "Monospace 16" \
+"Win + f" "Window Fullscreen" "#ffffff" "Monospace 16" \
+"Win + w" "Window Maximize" "#cccccc" "Monospace 16" \
+"Win + x" "Window Minimize" "#ffffff" "Monospace 16" \
+"Win + d" "Toggle Show Desktop" "#cccccc" "Monospace 16" \
+"Win + e" "Window Begin Move" "#ffffff" "Monospace 16" \
+"Win + r" "Window Begin Resize" "#cccccc" "Monospace 16" \
+"Win + t" "Window Always on Top" "#ffffff" "Monospace 16" \
+"Win + b" "Window Always on Bottom" "#cccccc" "Monospace 16" \
+"Win + y" "Window Toggle Shade" "#ffffff" "Monospace 16" \
+"Win + n" "Window Toggle Show or Hide Decoration" "#cccccc" "Monospace 16" \
+"Win + z" "Window Toggle Raise or Lower" "#ffffff" "Monospace 16" \
+"Win + m" "Window Move Center" "#cccccc" "Monospace 16" \
+"Win + ;" "Window Decrease Opacity" "#ffffff" "Monospace 16" \
+"Win + '" "Window Increase Opacity" "#cccccc" "Monospace 16" \
+"Win + Esc" "Window Switch Previous" "#ffffff" "Monospace 16" \
+"Alt + Esc" "Window Switch Next" "#cccccc" "Monospace 16" \
+"Win + grave" "Overview (Workspace List)" "#ffffff" "Monospace 16" \
+"Win + Tab" "Expose (Window List)" "#cccccc" "Monospace 16" \
+"Win + Mouse Left Drag" "Window Move" "#ffffff" "Monospace 16" \
+"Win + Mouse Right Drag" "Window Resize" "#cccccc" "Monospace 16" \
+"Win + a" "Window Switch Previous" "#ffffff" "Monospace 16" \
+"Win + s" "Window Switch Next" "#cccccc" "Monospace 16" \
+"Alt + a" "Workspace Switch Previous" "#ffffff" "Monospace 16" \
+"Alt + s" "Workspace Switch Next" "#cccccc" "Monospace 16" \
+"Alt + Ctrl + 1-9" "Workspace Switch to 1-9" "#ffffff" "Monospace 16" \
+"Alt + Shift + 1-9" "Window Send to 1-9" "#cccccc" "Monospace 16" \
+"Print" "Screenshot" "#ffffff" "Monospace 16" \
+"Win + Print" "Screenshot Current Window" "#cccccc" "Monospace 16" \
+"Ctrl + Print" "Screenshot Selected Area" "#ffffff" "Monospace 16" \
+"Fn + F5 (XF86MonBrightnessDown)" "Brightness Decrease" "#cccccc" "Monospace 16" \
+"Fn + F6 (XF86MonBrightnessUp)" "Brightness Increase" "#ffffff" "Monospace 16" \
+"Alt + Shift + [" "Brightness Decrease" "#cccccc" "Monospace 16" \
+"Alt + Shift + ]" "Brightness Increase" "#ffffff" "Monospace 16" \
+"Alt + Shift + v" "Launch Volume Control (pavucontrol)" "#cccccc" "Monospace 16" \
+"Fn + F10 (XF86AudioMute)" "Volume Toggle Mute" "#ffffff" "Monospace 16" \
+"Fn + F11 (XF86AudioLowerVolume)" "Volume Decrease" "#cccccc" "Monospace 16" \
+"Fn + F12 (XF86AudioRaiseVolume)" "Volume Increase" "#ffffff" "Monospace 16" \
+"Alt + m" "Volume Toggle Mute" "#cccccc" "Monospace 16" \
+"Alt + Shift + ," "Volume Decrease" "#ffffff" "Monospace 16" \
+"Alt + Shift + ." "Volume Increase" "#cccccc" "Monospace 16" \
+"Alt + Ctrl + ," "Volume Decrease Slowly" "#ffffff" "Monospace 16" \
+"Alt + Ctrl + ." "Volume Increase Slowly" "#cccccc" "Monospace 16" \
---
+++ project/gen/hotkey-tips/share/locale/en_US/view/gen-md.view.php
@@ -0,0 +1,10 @@
+
+
+# Popos Cosmic Keybindings
+
+
+## Hotkey
+
+| Keybind |	Action |
+| ------- | ------ |
+<?php echo($_Data_['List']); ?>
